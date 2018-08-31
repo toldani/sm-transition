43dcc529c56de266ff9e22d3c9ffbf179d24583f
toldani <toldani@gmail.com>
2018-08-30 21:01:42 -0400

    simplified rquote replace

--- sql_helpers.rb
+++ sql_helpers.rb
@@ -10,7 +10,7 @@ require 'fileutils' # File utilities
 require 'cgi' # Misc. network functions
 
 # Regular expression for matching [rquote] bbcode tags
-RQUOTE_RX = /\[rquote\=(?<pid>\d+)&amp;tid=(?<tid>\d+)&amp;author=(?<username>.*?)\](?<contents>.*)\[\/rquote\]/m
+RQUOTE_RX = /\[rquote\=(\d+)&amp;tid=(\d+)&amp;author=(.*?)\]/
 
 # The XMB database, running in a virtual machine.  Port forwarding is set up to allow communication
 XMB_DB = Mysql2::Client.new(host: "127.0.0.1", username: "root", password: "science", port: 3333, database: "science_xmb1")
@@ -31,12 +31,11 @@ end
 
 # return a post with all the rquote tags fixed and replaced with phpbb quote tags
 def rquote_fix(post)
-	m = RQUOTE_RX.match(post)
-	if m.nil?
-		return post
-	else
-		qp = XMB_POSTS[m['pid']]
-		u = XMB_USERS.find_by('username', m['username'])
-		return post.sub(m[0], "[quote=#{m['username']} post_id=#{m['pid']} time=#{qp['dateline']} user_id=#{u['uid']}]#{m['contents']}[/rquote]")
-	end
+  str = post.gsub(RQUOTE_RX) do |m|
+  	pid, tid, username = RQUOTE_RX.match(m).captures
+  	post_time = XMB_POSTS[pid]['dateline']
+  	uid = XMB_USERS.find_by('username', username)['uid']
+  	"[quote=#{username} post_id=#{pid} time=#{post_time} user_id=#{uid}]"
+  end
+  str.gsub("[/rquote]", "[/quote]")
 end
