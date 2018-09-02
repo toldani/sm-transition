1833b9f871ce6ec156267a1e7817efc658cb5608
toldani <toldani@gmail.com>
2018-09-02 04:15:51 -0400

    another attempt at a fix

--- sql_helpers.rb
+++ sql_helpers.rb
@@ -11,12 +11,13 @@ require 'cgi' # Misc. network functions
 
 # The following maps the XMB member fields to their corresponding fields in sm_users. Note that this only contains the
 # fields that can be moved with no changes necessary.
-USERS_MAP = {"uid" => "user_id", "username" => "username", "regip" => "user_ip", "regdate" => "user_regdate",
+USERS_MAP = Hash.new("").merge {"uid" => "user_id", "username" => "username", "regip" => "user_ip", "regdate" => "user_regdate",
 	"lastvisit" => "user_lastvisit", "email" => "user_email", "password" => "user_password", "postnum" => "user_posts",
-	"sig" => "user_sig", "bday" => "user_birthday" }
+	"sig" => "user_sig", "bday" => "user_birthday"}
 
 # The following maps the XMB fields to their corresponding fileds in sm_profile_fields_data
-PROFILE_FIELDS_DATA_MAP = {"uid" => "user_id", "location" => "pf_phpbb_location", "site" => "pf_phpbb_website", "mood" => "pf_mood", "bio" => "pf_bio"}
+PROFILE_FIELDS_DATA_MAP = Hash.new("").merge {"uid" => "user_id", "location" => "pf_phpbb_location", "site" => "pf_phpbb_website",
+	"mood" => "pf_mood", "bio" => "pf_bio"}
 
 # Regular expression for matching [rquote] bbcode tags
 RQUOTE_RX = /\[rquote\=(\d+)&amp;tid=(\d+)&amp;author=(.*?)\]/
@@ -51,8 +52,8 @@ P_POSTS = PTable.new('sm_posts')
 
 # generates the email hash that's stored in the phpbb users table
 def email_hash(email)
-	return nil if email.nil?
-  Digest::CRC32.checksum(email.downcase).to_s + email.length.to_s
+	return nil if email.nil? || email.empty?
+  (Digest::CRC32.checksum(email.downcase).to_s + email.length.to_s).to_i
 end
 
 # return a post with all the rquote tags fixed and replaced with phpbb quote tags
@@ -70,7 +71,7 @@ def sql_clean(v)
 	if v.is_a?(Numeric)
 		return v.to_s
 	elsif v.is_a?(String)
-		return "'#{v}'"
+		return "'#{XMB_DB.escape(v)}'"
 	end
 end
 
@@ -87,6 +88,8 @@ end
 def convert_xmb_user(u,debug=false)
 	if u.is_a?(Integer)
 		u = X_MEMBERS[u]
+	elsif u.is_a?(String)
+		u = X_MEMBERS.find_by('username', u)
 	end
 
 	user = {"username_clean" => u['username'].downcase, "user_email_hash" => email_hash(u['email']), "user_permissions" => ""}
@@ -95,7 +98,8 @@ def convert_xmb_user(u,debug=false)
 	if user['username'].to_s == ""
 		puts "User ##{user['user_id']} is null. Aborting.\n"
 	else
-		pfd = {}
+		fields = %w"interests occupation location youtube facebook skype twitter website"
+		pfd = fields.inject({}) {|h,s| h.merge("pf_phpbb_#{s}" => "")}
 		PROFILE_FIELDS_DATA_MAP.each_pair {|k,v| pfd[v] = u[k]}
 
 		if debug
@@ -103,8 +107,8 @@ def convert_xmb_user(u,debug=false)
 			puts pfd
 			return [user, pfd]
 		else
-			insert_phpbb_row('sm_users2', user)
-			insert_phpbb_row('sm_profile_fields_data', pfd)
+			puts insert_phpbb_row('sm_users2', user)
+			puts insert_phpbb_row('sm_profile_fields_data', pfd)
 		end
 	end
 end
