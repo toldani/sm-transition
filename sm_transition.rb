bacb6037d0cdf17ee745c76d58d72da8fb540183
toldani <toldani@gmail.com>
2018-09-14 19:51:14 -0400

    bugfixes

--- sm_transition.rb
+++ sm_transition.rb
@@ -27,18 +27,18 @@ X2P_FID = {2=>5, 11=>21, 3=>12, 13=>1, 5=>7, 6=>9, 7=>18, 9=>11, 10=>6, 12=>8, 1
 				19=>19, 20=>16, 22=>15, 23=>14, 24=>10, 8=>20}
 
 @table_class = Hash.new(SQLTable)
-@table_class.merge("users" => UserTable, "members" => UserTable, "posts" => PostTable)
+@table_class.merge!("users" => UserTable, "members" => UserTable, "posts" => PostTable)
 
 # Automatically initialize a SQLTable object for each table in the XMB db
 XMB = XMB_TABLES.inject({}) do |h,t|
 	n = t[/(?<=xmb_).+/]
-	h.merge(n => @table_class[n])
+	h.merge(n => @table_class[n].new(t))
 end
 
 # Automatically initialize a SQLTable object for each table in the PHPBB db
 PHPBB = PHPBB_TABLES.inject({}) do |h,t|
 	n = t[/(?<=sm_).+/]
-	h.merge(n => @table_class[n])
+	h.merge(n => @table_class[n].new(t))
 end
 
 
