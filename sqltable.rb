6997438943d3169181035084cad0fb6832452f2f
toldani <toldani@gmail.com>
2018-09-15 07:44:39 -0400

    bugfixes

--- sqltable.rb
+++ sqltable.rb
@@ -92,7 +92,7 @@ class SQLTable
 
 		r.each_pair do |t,h|
 			h.delete_if {|k,v| v.nil?}
-			vstring = h.values.map {|g| self.sanitize(g) * ', '}
+			vstring = h.values.map {|g| self.sanitize(g).to_s * ', '}
 			kstring = h.keys * ', '
 			q = "INSERT INTO #{t} (#{kstring}) VALUES (#{vstring})"
 			puts @db.query(q)
