a089dceb0e17744b6505187895c75d5349487d92
toldani <toldani@gmail.com>
2018-09-16 20:54:39 -0400

    ordinary bugfix

---
+++ import_posts.rb
@@ -0,0 +1,10 @@
+require './sm_transition.rb'
+
+XMB.threads.each do |t|
+  plist = XMB.posts.where(tid: t['tid']).sort_by {|i| i[pid]}
+  topic = XMB.threads.to_phpbb(t, plist)
+  SQLTable.insert_record(topic)
+  puts "Topic added, #{plist.length} posts found.  Adding them to posts table..."
+
+  plist.each {|p| SQLTable.insert_record(XMB.posts.to_phpbb(p))}
+end
