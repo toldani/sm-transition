c38df84076683af80b65e838b57755299c8ac17f
Tom Oldani <toldani@gmail.com>
2018-09-20 07:14:47 -0400

    nothing special with this commit

--- import_posts.rb
+++ import_posts.rb
@@ -1,9 +1,9 @@
 require './sm_transition.rb'
 
-tmax = PHPBB.topics.max
+# tmax = PHPBB.topics.max
 
-PHPBB_DB.query("DELETE FROM sm_topics WHERE topic_id = #{tmax}")
-PHPBB_DB.query("DELETE FROM sm_posts WHERE topic_id = #{tmax}")
+# PHPBB_DB.query("DELETE FROM sm_topics WHERE topic_id = #{tmax}")
+# PHPBB_DB.query("DELETE FROM sm_posts WHERE topic_id = #{tmax}")
 
 XMB.threads.each do |t|
   plist = XMB.posts.to_phpbb(t['tid'])  #where(tid: t['tid']).sort_by {|i| i['pid']}
