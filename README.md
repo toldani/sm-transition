24511c93d398b12d36f110d36bdc479edb750883
Tom Oldani <toldani@gmail.com>
2018-09-28 15:12:11 -0400

    improved the readme to make the code easier to understand

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # sm-transition
 Scripts and SQL queries for transitioning the sciencemadness XMB database to phpBB
+
+The main file to load is "sm_transition.rb".  The easiest way to use it is to start up a Ruby console by running:
+
+pry -r ./sm-transition.rb
+
+This will connect both the XMB and phpBB databases and allow access to both of them.  To see what tables are available, you can type XMB_TABLES or PHPBB_TABLES in the Ruby console.  If you want to make an SQL query to either database, you can use these wrappers:
+
+XMB_DB.query("SELECT * FROM xmb_members WHERE uid = (SELECT MAX(uid) FROM xmb_members)").to_a.first
+PHPBB_DB.query("SELECT * FROM sm_users WHERE user_id = (SELECT MAX(user_id) FROM sm_users)").to_a.first
+
+These commands will return the most recently registered user in the XMB and phpBB databases, respectively. You don't have to do this in most cases though. Using the SQLTable class, you can do the same thing using the following commands:
+
+XMB.members[XMB.members.max]
+PHPBB.users[PHPBB.users.max]
+
+Individual records are always returned as Ruby hash objects.  Some tables that are especially important for converting from XMB to phpBB have ".to_phpbb(n)" methods, which will convert as many fields as possible and return a hash of tables that then need to be written to the phpBB database.  The XMB tables should not be written to for any reason.
+
+Files starting with "fix" or "import" are typically scripts that are made to perform various tasks that are necessary for converting the database to phpBB.  The X2P module contains a lot of useful methods for working with these databases.  New general-purpose methods are added there, and can be called without a reference to the X2P module.
+
+Importing posts takes quite a lot of time, so in order to speed it up, my conversion script will load all the data for a given thread/topic into memory, then process each one before writing it to the phpBB post table.
+
+For attachments, phpBB saves them as files in the regular filesystem, which seems preferable to saving them as large SQL fields.  I was curious as to how many attachments were duplicates in the system, and so set up a process where attachments were run through an MD5 checksum, then saved with their checksum as their filename.  This way, duplicate files would have the same name, and presumably also the same data.  The amount of duplication wasn't very high though, and implementing this only actually saved about 10% compared to not doing it.  
--- usertable.rb
+++ usertable.rb
@@ -2,16 +2,6 @@
 
 class UserTable < SQLTable
 
-	# The following maps the XMB member fields to their corresponding fields in sm_users. Note that this only contains the
-	# fields that can be moved with no changes necessary.
-	USERS_MAP = {"uid" => "user_id", "username" => "username", "regip" => "user_ip", "regdate" => "user_regdate",
-		"lastvisit" => "user_lastvisit", "email" => "user_email", "password" => "user_password", "postnum" => "user_posts",
-		"sig" => "user_sig", "bday" => "user_birthday"}	
-
-	# The following maps the XMB fields to their corresponding fileds in sm_profile_fields_data
-	PROFILE_FIELDS_DATA_MAP = {"uid" => "user_id", "location" => "pf_phpbb_location", "site" => "pf_phpbb_website", 
-    "mood" => "pf_mood", "bio" => "pf_bio"}
-
   # For tables of users, allows passing a string in square brackets to look up a row by username
 	def [](id)
 		if id.is_a?(String)
@@ -22,37 +12,49 @@ class UserTable < SQLTable
 	end
 
 	# put an XMB user record into a hash that can be inserted into the corresponding phpbb table
-	def import_user(id,debug=false)
+	def to_phpbb(id)
 		# if u.is_a?(Integer)
 		# 	u = self[u]
 		# elsif u.is_a?(String)
 		# 	u = self.find_by('username', u)
 		# end
-		u = self[id]
+		u = XMB.members[id]
 
     return nil if u.empty?
 
-		user = PHPBB.users.default_row.merge("username_clean" => u['username'].downcase, "user_email_hash" => email_hash(u['email']))
-		USERS_MAP.each_pair {|k,v| user[v] = u[k]}
-		
-		if user['username'].to_s == ""
-			puts "User ##{user['user_id']} is null. Aborting.\n"
-		else
-			fields = %w"interests occupation location youtube facebook skype twitter website"
-			pfd = fields.inject({}) {|h,s| h.merge("pf_phpbb_#{s}" => "")}
-			PROFILE_FIELDS_DATA_MAP.each_pair {|k,v| pfd[v] = u[k]}
+    user = {
+    	user_id: u['uid'],
+			user_type: 0,
+			group_id: 3,
+			user_permissions: "",
+			user_ip: u['regip'],
+			user_regdate: u['regdate'],
+			username: u['username'],
+			username_clean: u['username'].downcase,
+			user_password: u['password'],
+			user_email: u['email'],
+			user_email_hash: email_hash(u['email']),
+			user_birthday: u['bday'],
+			user_lastvisit: u['lastvisit'],
+			user_posts: u['postnum'],
+			user_sig: u['sig'].to_s
+		}
 
-			if debug
-				puts user
-				puts pfd
-				return [user, pfd]
-			else
-				# insert_phpbb_row('sm_users2', user)
-				# insert_phpbb_row('sm_profile_fields_data', pfd)
-				# puts [user, pfd] if user['user_id'] % 20 == 0
-				return {'sm_users' => user, 'sm_profile_fields_data' => pfd}
-			end
-		end
+		pfd = {
+			user_id: u['uid'],
+			pf_phpbb_interests: "",
+			pf_phpbb_occupation: "",
+			pf_phpbb_location: u['location'],
+			pf_phpbb_youtube: "",
+			pf_phpbb_facebook: "",
+			pf_phpbb_skype: "",
+			pf_phpbb_twitter: "",
+			pf_phpbb_website: u['site'],
+			pf_mood: u['mood'],
+			pf_bio: u['bio']
+		}
+
+		return {'sm_users' => user, 'sm_profile_fields_data' => pfd}
 	end
 
 private
